f2677d8f61991670a644634820a5454ac454ac94
Edgard Decena [Vauxoo] <edgard@vauxoo.com>
2022-07-02 14:49:17 -0400

    [UPD] calculator: update CI settings.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,7 +12,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.sumar(3, 2), 5)
 
     def test_multiplicar(self):
-        self.assertEqual(self.calculator.multiplicar(3, 4), 1)
+        self.assertEqual(self.calculator.multiplicar(3, 4), 12)
 
 
 if __name__ == "__main__":
